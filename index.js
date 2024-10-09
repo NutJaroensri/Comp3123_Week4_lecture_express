1327f0dbb5270bbcc16aa2c5b9cd9bfcbc65cdec
Nut Jaroensri <enyouthree@gmail.com>
2024-10-09 13:50:19 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const SERVER_PORT = process.env.port || 3000;
+
+// http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.get('/about', (req, res) => {
+    res.send('About us');
+})
+
+
+// http://localhost:3000/
+app.post('/', (req, res) => {
+    res.send('POST - Hello World');
+})
+
+// Listen to the server
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${SERVER_PORT}`);
+})
